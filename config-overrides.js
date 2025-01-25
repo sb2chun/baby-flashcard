8e254bd6e060fa60952ef309f28bfae2e0c916fd
sb2chun <sb2chun@gmail.com>
2025-01-26 02:08:00 +0900

    1. Imaeg upload - 직업, 동물, 악기, 교통수단 2. 소스 Hide 추가

---
+++ config-overrides.js
@@ -0,0 +1,25 @@
+const WebpackObfuscator = require('webpack-obfuscator');
+
+module.exports = function override(config, env) {
+  if (env === 'production') {
+    config.plugins.push(new WebpackObfuscator({
+      compact: true,
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 0.75,
+      deadCodeInjection: true,
+      deadCodeInjectionThreshold: 0.4,
+      debugProtection: true,
+      disableConsoleOutput: true,
+      identifierNamesGenerator: 'hexadecimal',
+      rotateUnicodeArray: true,
+      selfDefending: true,
+      shuffleStringArray: true,
+      splitStrings: true,
+      splitStringsChunkLength: 10,
+      stringArray: true,
+      stringArrayEncoding: ['base64'], // 배열로 변경
+      stringArrayThreshold: 0.75
+    }));
+  }
+  return config;
+};
