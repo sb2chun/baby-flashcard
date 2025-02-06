d4544d85ecf4cb9432d43edc5be3f059cb406f4e
sb2chun <sb2chun@gmail.com>
2025-02-07 01:14:49 +0900

    overflow test - except hfull

--- src/components/FlashcardApp/MainContent.jsx
+++ src/components/FlashcardApp/MainContent.jsx
@@ -31,7 +31,7 @@ const MainContent = ({
       className={`flex-1 mt-[10vh] mt-[10vh] md:mt-[10vh] h-[90vh] md:h-[90vh] overflow-hidden
       ${isFullscreen ? "fixed inset-0 z-20 bg-white mt-[10vh] h-[90vh] w-screen" : ""}`}
     >
-      <div className="relative h-full flex flex-col items-center justify-center p-4">
+      <div className="relative  flex flex-col items-center justify-center p-4">
         <div className="relative w-full h-[50vh] md:h-[60vh] flex items-center justify-center">
           <button
             onClick={() => handleCardChange(currentIndex - 1)}
